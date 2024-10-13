e00ca1675c3cd6fc0036c29e0176cf12db4f6e56
AgustinaBosiaa <2324696@ucc.edu.ar>
2024-10-13 20:27:07 -0300

    cambios

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
+/*Ejercicio N°2
+Sistema de gestión de empleados. Usar HashMap para almacenar y gestionar empleados por
+su número de identificación (ID).
+● La clave será el número de identificación del empleado y el valor será su nombre.
+● Implementa un menú con opciones para:
+● Agregar un nuevo empleado.
+● Buscar un empleado por su número de identificación.
+● Eliminar un empleado del sistema.
+● Imprimir la lista completa de empleados.
+*/
 
 int main() {
 
